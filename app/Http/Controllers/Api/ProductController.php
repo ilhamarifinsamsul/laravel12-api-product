407d27e4b9b5cc37337d357d07fc6c0732d3812c
ilhamsamsularifin <ilhamsamsularifin@gmail.com>
2025-06-26 11:17:41 +0700

    add displaydata product and create data product

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ProductController extends Controller
+{
+    /**
+     * index
+     * 
+     * @return void
+     */
+
+    public function index()
+    {
+        //get all products
+        $products = Product::latest()->paginate(5);
+
+        //return collection of products as a resource
+        return new ProductResource(true, 'List Data Products', $products);
+    }
+
+    /**
+      * store
+      *
+      * @param mixed $request
+      * @return void
+      */
+
+    public function store(Request $request)
+    {
+        // define validation rules
+        $validator = Validator::make($request->all(), [
+            'image' => 'required|image|mimes:jpg,jpeg,png,svg|max:2048',
+            'title' => 'required',
+            'description' => 'required',
+            'price' => 'required|numeric',
+            'stock' => 'required|numeric',
+        ]);
+
+        // check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // upload image
+        $image = $request->file('image');
+        $image->storeAs('products', $image->hashName());
+
+        // create product
+        $product = Product::create([
+            'image' => $image->hashName(),
+            'title' => $request->title,
+            'description' => $request->description,
+            'price' => $request->price,
+            'stock' => $request->stock,
+        ]);
+
+        // return response
+        return new ProductResource(true, 'Data Product Berhasil Ditambahkan!', $product);
+    }
+}
