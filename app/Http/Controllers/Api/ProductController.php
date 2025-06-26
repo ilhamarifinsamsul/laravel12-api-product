8a7e185b3da8e41db75f522d802bbeed97dfcb19
ilhamsamsularifin <ilhamsamsularifin@gmail.com>
2025-06-26 13:38:44 +0700

    add function update product

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -8,6 +8,7 @@ use App\Models\Product;
 use GuzzleHttp\Handler\Proxy;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
@@ -80,5 +81,64 @@ class ProductController extends Controller
 
         // return single product as a resource
         return new ProductResource(true, 'Detail Data Product!', $product);
+    }
+
+    /**
+     * update
+     * 
+     * @param mixed $request
+     * @param mixed $id
+     * @return void
+     */
+
+    public function update(Request $request, $id)
+    {
+        // find product by id
+        $product = Product::findOrFail($id);
+
+        // define validation rules
+        $validator = Validator::make($request->all(), [
+            'image' => 'image|mimes:jpg,jpeg,png,svg|max:2048',
+            'title' => 'required',
+            'description' => 'required',
+            'price' => 'required|numeric',
+            'stock' => 'required|numeric',
+        ]);
+
+        // check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // check if image is not empty
+        if ($request->file('image')) {
+            
+            // delete old image
+            Storage::delete('products/' . basename($product->image));
+
+            // upload new image
+            $image = $request->file('image');
+            $image->storeAs('products', $image->hashName());
+
+            // update product with new image
+            $product->update([
+                'image' => $image->hashName(),
+                'title' => $request->title,
+                'description' => $request->description,
+                'price' => $request->price,
+                'stock' => $request->stock,
+            ]);
+        } else {
+            // update product without new image
+            $product->update([
+                'title' => $request->title,
+                'description' => $request->description,
+                'price' => $request->price,
+                'stock' => $request->stock,
+            ]);
+        }
+
+        // return response
+        return new ProductResource(true, 'Data Product Berhasil diupdate!', $product);
     }
 }
